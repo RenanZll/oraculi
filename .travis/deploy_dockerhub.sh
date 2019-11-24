a4787b31358a696bdf01b26dfdedf2080331f706
Renan Zelli <renanzelli@hotmail.com>
2019-11-24 18:00:02 -0300

    Show docker images for travis debugging

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,10 +5,13 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
   docker login --username $DOCKER_USER --password $DOCKER_PASS
 
   echo "Delete __pycache__ files created by Docker -->"
-  find . -name "__pycache__" | sudo xargs rm -rf
+  find . -name "__pycache__" | sudo xargs rm -rf && echo "Files deleted!"
 
   echo "Docker build -->"
   docker build -f Dockerfile -t oraculi:lastest -t oraculi:v$TRAVIS_COMMIT .
+
+  echo "Docker images command -->"
+  docker images
 
   echo "Docker tag -->"
   docker tag  oraculi $DOCKER_REPO

f13a7b3e985cf95f7389cb6ee489128e0826948b
Renan Zelli <renanzelli@hotmail.com>
2019-11-24 12:39:44 -0300

    Try to run docker hub deploy on travis

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,4 +1,6 @@
-if ["$TRAVIS_BRANCH" = "master"]; then
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
   docker login --username $DOCKER_NAME --password $DOCKER_PASS
   docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:lastest .
   docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO

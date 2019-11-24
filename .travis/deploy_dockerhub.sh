5c753b31ed3c718bc669377c7f127a1435334283
Renan Zelli <renanzelli@hotmail.com>
2019-11-24 12:45:14 -0300

    Debbuging in travis

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
+  echo "Docker login -->"
   docker login --username $DOCKER_NAME --password $DOCKER_PASS
+  echo "Docker build -->"
   docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:lastest .
+  echo "Docker tag -->"
   docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
+  echo "Docker push -->"
   docker push $DOCKER_REPO
 fi

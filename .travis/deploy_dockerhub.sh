ec80bf280336498a723a3d242a6a00b25af50429
Renan Zelli <renanzelli@hotmail.com>
2019-11-24 17:46:34 -0300

    Travis deletes docker generated files before the docker hub push

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -3,10 +3,16 @@
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   echo "Docker login -->"
   docker login --username $DOCKER_USER --password $DOCKER_PASS
+
+  echo "Delete __pycache__ files created by Docker -->"
+  find . -name "__pycache__" | sudo xargs rm -rf
+
   echo "Docker build -->"
   docker build -f Dockerfile -t oraculi:lastest -t oraculi:v$TRAVIS_COMMIT .
+
   echo "Docker tag -->"
   docker tag  oraculi $DOCKER_REPO
+
   echo "Docker push -->"
   docker push $DOCKER_REPO
 fi

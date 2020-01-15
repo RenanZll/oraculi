e813c170818e214ce9ce552bba7f9dc7a8209f99
Renan Zelli <renanzelli@hotmail.com>
2020-01-15 01:07:31 -0300

    Add memoization to speed up app launch

--- oraculi.py
+++ oraculi.py
@@ -3,16 +3,25 @@ from model.prediction import Prediction
 import json
 import os
 
+class Memoize:
+
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+            self.memo[args] = self.fn(*args)
+        return self.memo[args]
+
 def create_app():
     flask_app = Flask(__name__)
     return flask_app
 
+@Memoize
 def load_model():
     return Prediction()
 
-print("Start model loading...")
-prediction = load_model()
-print("Model loaded!")
 app = create_app()
 print("App created!")
 
@@ -23,6 +32,7 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     document = request.json['document']
+    prediction = load_model()
     return json.dumps(
             {'classification': prediction.to(document)}
             , ensure_ascii=False)
